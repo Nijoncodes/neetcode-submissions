4a00ea1c8b8170da3bf3b4637309e1958a3fa646
Nijoncodes <117867635+Nijoncodes@users.noreply.github.com>
2026-04-21 14:27:44 -0500

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+
+integer_type = 68
+float_type = 56.8
+bool_type = True 
+string_type = "LelandManigo"
+list_type = ["RayGun", "Spaz 12", "Armmagedon"]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
