efc964e2ae6d0cfb3214d1416a1495b3ac8394e0
Nijoncodes <117867635+Nijoncodes@users.noreply.github.com>
2026-06-01 21:51:44 -0500

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,20 @@
+variable = 10
+
+print(type(variable))
+
+variable = 5.6
+
+
+print(type(variable))
+
+variable = True 
+
+print(type(variable))
+
+variable = "Travis"
+
+print(type(variable))
+
+variable = []
+
+print(type(variable))
