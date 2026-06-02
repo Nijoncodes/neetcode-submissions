8f472b20b56b430afa059f11ef45a36cd4c6295c
Nijoncodes <117867635+Nijoncodes@users.noreply.github.com>
2026-06-02 13:41:43 -0500

    Add: python-multiple-parameters - submission-1

---
+++ Python For Beginners/python-multiple-parameters/submission-1.py
@@ -0,0 +1,18 @@
+
+def two_sum(num1, num2):
+    return num1 + num2 
+
+
+def three_sum(num1, num2, num3):
+    return num1 + num2 + num3
+
+
+
+
+# do not modify below this line
+print(two_sum(7, 10))
+print(three_sum(3, 5, 8))
+print(two_sum(10, 9))
+print(three_sum(5, 14, 6))
+
+
