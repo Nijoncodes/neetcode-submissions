91760b1e9569f0daac3a02cbf1de4dd2551e2b6f
Nijoncodes <117867635+Nijoncodes@users.noreply.github.com>
2026-06-01 21:49:27 -0500

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,16 @@
+
+integer_type = 25
+float_type = 98.6
+bool_type = True 
+string_type = "She was moving to fast I had to put it in park"
+list_type = ["Ray Gun", "HK-21"]
+
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
