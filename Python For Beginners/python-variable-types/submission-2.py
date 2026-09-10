3414debccdf2c9b1f713a7a48cf82abd86441c73
Nijoncodes <117867635+Nijoncodes@users.noreply.github.com>
2026-09-10 10:05:17 -0500

    Add: python-variable-types - submission-2

---
+++ Python For Beginners/python-variable-types/submission-2.py
@@ -0,0 +1,15 @@
+integer_type = 25
+float_type = 9.7
+bool_type = True 
+string_type = "Niko Belic"
+list_type = ["Special Carbine", "AK-47", "MP5"]
+
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
