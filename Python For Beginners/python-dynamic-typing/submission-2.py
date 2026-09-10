22331247fe40bf16f4126645cd094496a7ac4192
Nijoncodes <117867635+Nijoncodes@users.noreply.github.com>
2026-09-10 10:06:50 -0500

    Add: python-dynamic-typing - submission-2

---
+++ Python For Beginners/python-dynamic-typing/submission-2.py
@@ -0,0 +1,19 @@
+variable = 15
+
+print(type(variable))
+
+variable = 5.7
+
+print(type(variable))
+
+variable = False
+
+print(type(variable))
+
+variable = "Niko Belic"
+
+print(type(variable))
+
+variable = ["AK-47", "Glock"]
+
+print(type(variable))
